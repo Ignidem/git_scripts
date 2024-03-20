349f70b0bf713b594f71f080996e175562566d1a
Eivlisskiv <gautma13@gmail.com>
2024-03-19 22:13:24 -0400

    updated commit_all shell script to use better git commands;

--- commit_all.sh
+++ commit_all.sh
@@ -23,10 +23,11 @@ commit(){
 	git commit -m "$message"
 	
 	if [[ ! -z "$2" ]]; then
-		origin=$(git remote -v | awk '$3=="(push)" {print $1}')
+		#origin=$(git remote -v | awk '$3=="(push)" {print $1}')
+		origin=$(git remote)
 		branch=$2
 		if [[ $2 == "--current" ]]; then 
-			branch=$(git rev-parse --abbrev-ref HEAD)
+			branch=$(git symbolic-ref --short HEAD)
 		fi
 		git push $origin $branch
 	fi
