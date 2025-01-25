0781774b1d8a58da061a255f1303a074600cc133
Eivlisskiv <gautma13@gmail.com>
2025-01-24 21:22:56 -0500

    pushall added recursive submodule

--- commit_all.sh
+++ commit_all.sh
@@ -1,15 +1,41 @@
+submodules(){
+	local path="$1"
+	local branch="$2"
+	gitmodules='./.gitmodules'
+	
+	echo "Checking for submodules in $path"
+	
+	if [[ ! -f $gitmodules ]]; then
+		echo "No submodules found in $path"
+		return 1
+	fi
+	
+	grep path $gitmodules | sed 's/.*= //' | while read -r p
+	do
+		echo "Found submodule $p"
+		commit "${path}/${p}/" "$branch"
+		echo "Returning to Parent module $path"
+		cd "$path" #return to parent path after each iteration
+	done
+}
+
 commit(){
-	cd "$1"
+	local path="$1"
+	local branch="$2"
+	
+	cd "$path"
+	local name="${PWD##*/}" #get the name of the git repo's directory to use as name
+	
+	submodules "$1" "$2"
 	echo "------------------"
-	dir="${PWD##*/}"
-	echo $dir
+	echo "QuickPushing $name from $path on branch $branch...";
 	git status
 		
 	if [[ ! `git status --porcelain` ]]; then
 		return 1
 	fi
 	
-	read -rep "Enter message to add all files and commit $dir, or [--exit] to stop the process."$'\n>' message < /dev/tty
+	read -rep "Enter message to add all files and commit $name, or [--exit] to stop the process."$'\n>' message < /dev/tty
 	
 	if [[ "$message" == "--exit" ]]; then
 		exit -1
@@ -33,13 +59,6 @@ commit(){
 	fi
 }
 
-origin_dir="$1"
-branch="$2"
-grep path './.gitmodules' | sed 's/.*= //' | while read -r p
-do
-	commit "${origin_dir}/${p}/" "$branch"
-done
-
-commit "$origin_dir" "$branch"
+commit "$1" "$2" ''
 
 #git config --global alias.commitall '!sh "/../commit_all.sh" "$(realpath ${GIT_PREFIX:-.})"'
